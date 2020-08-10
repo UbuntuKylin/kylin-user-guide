c041abae619f1d4e4237b6b31e5ddf1930cda58f
handsome-feng <jianfengli@ubuntukylin.com>
2020-08-10 07:32:37 +0000

    Update the files name, copyright, manpage

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,10 @@
 /*
- * Copyright (C) 2019 National University of Defense Technology(NUDT) & Kylin Ltd.
+ * Copyright (C) 2020, KylinSoft Co., Ltd.
  *
- * Authors:
- *  xiaoyi wu    wuxiaoyi@kylinos.cn
- *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 3.
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +12,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <QApplication>
